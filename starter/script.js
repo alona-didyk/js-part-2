57ed81c08a97a7f0136bf84d8766c5ef022ad6a4
Alona Didyk <alionadiduk31@gmail.com>
2023-10-26 17:12:57 +0300

    new commit

---
+++ starter/script.js
@@ -0,0 +1,10 @@
+"use strict";
+// forbids us to do sertain things; it creates a visible erors
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive");
+
+// const interface = "Audio"; //strict mode reserved word
